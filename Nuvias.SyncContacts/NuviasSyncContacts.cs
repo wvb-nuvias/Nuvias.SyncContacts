6ae957d6c4e3f90ff61056353c9bb036bfad97c4
Wouter Vanbelleghem <wouter.vanbelleghem@nuvias.com>
2022-01-13 14:18:31 +0100

    Builded for install

--- Nuvias.SyncContacts/NuviasSyncContacts.cs
+++ Nuvias.SyncContacts/NuviasSyncContacts.cs
@@ -1,16 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Diagnostics;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Console;
-using Microsoft.Extensions.Logging.EventLog;
 using System.Configuration;
 using System.Net.NetworkInformation;
 using System.Globalization;
